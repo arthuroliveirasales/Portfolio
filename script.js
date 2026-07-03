c05c3bf6c775d37aae0e724155e2a94506ce7f32
arthuroliveirasales <arthuroliveirasales60@gmail.com>
2026-07-03 17:39:02 -0300

    feat: add full responsiveness feat: add a mobile menu feat: add animation to the home section

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+//translate
 async function changeLanguage(language) {
+  const response = await fetch(`languages/${language}.json`);
 
-    const response = await fetch(`languages/${language}.json`);
+  const texts = await response.json();
 
-    const texts = await response.json();
+  document.querySelectorAll("[data-lang]").forEach((element) => {
+    const key = element.dataset.lang;
 
-    document.querySelectorAll("[data-lang]").forEach(element => {
-
-        const key = element.dataset.lang;
-
-        element.innerHTML = texts[key];
-    });
+    element.innerHTML = texts[key];
+  });
 }
 
+//mobile menu
+const menuBtn = document.querySelector("#menuBtn");
+const menuContainer = document.querySelector(".menuContainer");
+
+function menuView() {
+  menuContainer.classList.toggle("visible");
+  if (menuContainer.classList.contains("visible")) {
+    menuBtn.textContent = "✕";
+  } else {
+    menuBtn.textContent = "☰";
+  }
+}
